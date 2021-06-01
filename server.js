1f6fc871158e27427e39cd0d200c9e5092e8ef06
zulvkr <ivan.zulf.ikar@gmail.com>
2021-06-02 06:05:01 +0700

    git works

--- server.js
+++ server.js
@@ -2,9 +2,52 @@ const express = require('express')
 const mongo = require('mongodb')
 
 const app = express()
+app.use(express.json())
 
-app.post('/trip', (req, res) => {})
-app.get('/trips', (req, res) => {})
+const url = 'mongodb://localhost:27017'
+
+let db, trips, expenses
+
+mongo.connect(
+  url,
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  },
+  (err, client) => {
+    if (err) {
+      console.err(error)
+      return
+    }
+    db = client.db('tripcost')
+    trips = db.collection('trips')
+    expenses = db.collection('expenses')
+  }
+)
+
+app.post('/trip', (req, res) => {
+  const name = req.body.name
+  trips.insertOne({ name: name }, (err, result) => {
+    if (err) {
+      console.error(err)
+      res.status(500).json({ err: err })
+      return
+    }
+    console.log(result)
+    res.status(200).json({ ok: true })
+  })
+})
+
+app.get('/trips', (req, res) => {
+  trips.find().toArray((err, items) => {
+    if (err) {
+      console.error(err)
+      res.status(500).json({ err: err })
+      return
+    }
+    res.status(200).json({ trips: items })
+  })
+})
 app.post('/expense', (req, res) => {})
 app.get('/expenses', (req, res) => {})
 
