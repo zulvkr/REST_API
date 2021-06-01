38577f711560765d29116b4ec9f37d7d8599adc0
zulvkr <ivan.zulf.ikar@gmail.com>
2021-06-02 06:41:20 +0700

    psoting expense works

--- server.js
+++ server.js
@@ -48,7 +48,25 @@ app.get('/trips', (req, res) => {
     res.status(200).json({ trips: items })
   })
 })
-app.post('/expense', (req, res) => {})
+app.post('/expense', (req, res) => {
+  expenses.insertOne(
+    {
+      trip: req.body.trip,
+      date: req.body.date,
+      amount: req.body.amount,
+      category: req.body.category,
+      description: req.body.description
+    },
+    (err, result) => {
+      if (err) {
+        console.error(err)
+        res.status(500).json({ err: err })
+        return
+      }
+      res.status(200).json({ ok: true })
+    }
+  )
+})
 app.get('/expenses', (req, res) => {})
 
 app.listen(3000, () => console.log('Server ready'))
