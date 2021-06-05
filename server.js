53231f29236e6550d5204310d28f711b5bf5964b
zulvkr <ivan.zulf.ikar@gmail.com>
2021-06-05 22:39:19 +0700

    async post trip

--- server.js
+++ server.js
@@ -25,21 +25,20 @@ mongo.connect(
   }
 )
 
-app.post('/trip', (req, res) => {
-  const name = req.body.name
-  trips.insertOne({ name: name }, (err, result) => {
-    if (err) {
-      console.error(err)
-      res.status(500).json({ err: err })
-      return
-    }
+app.post('/trip', async (req, res) => {
+  try {
+    const name = req.body.name
+    const result = await trips.insertOne({ name: name })
+
     console.log(result)
     res.status(200).json({ ok: true })
-  })
+  } catch (err) {
+    console.error(err)
+    res.status(500).json({ err: err })
+  }
 })
 
 app.delete('/trip', async (req, res) => {
-  
   try {
     const id = new mongo.ObjectId(req.body.id)
     const idIsExist = await trips.findOne({ _id: id })
@@ -54,7 +53,6 @@ app.delete('/trip', async (req, res) => {
 
     console.log(result)
     res.status(200).json({ ok: result })
-
   } catch (error) {
     console.error(error)
     res.status(500).json({ err: error })
